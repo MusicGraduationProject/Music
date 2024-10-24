5874fd21bc671decb625cda8f7470c302366a7e1
sunshiningsoo <yenkee1219@gmail.com>
2024-10-24 15:05:07 +0900

    add: project init

---
+++ Music/Music/MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicApp.swift
+//  Music
+//
+//  Created by 박성수 on 10/24/24.
+//
+
+import SwiftUI
+
+@main
+struct MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
